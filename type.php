a0405b2e322839c75d79634e27fa2fce0c542c2d
Anuz <ceanuz66@gmail.com>
2023-04-03 08:14:10 +0600

    basic type

--- type.php
+++ type.php
@@ -6,6 +6,12 @@ $a_str = "hi";
 $a_str2 = 'hello';
 $a_int = 10;
 
-echo get_debug_type($a_bool), "\n", "type";
+echo "type of $a_bool is: ", get_debug_type($a_bool);
+
+if(is_bool($a_bool)){
+    echo "Boolean", "<br>";
+}
+
+var_dump($a_int);
 
 ?>
